06695be8301507913e8365d9bf8bc4004d454cbb
Eimen Kiose <161055517+xek1923x@users.noreply.github.com>
2025-12-26 12:12:56 +0100

    Add encoder and media keys support to main.py
    Refactor keyboard setup to include encoder and media keys.

--- Firmware/main.py
+++ Firmware/main.py
@@ -1,35 +1,54 @@
-# You import all the IOs of your board
 import board
-
-# These are imports from the kmk library
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.scanners.keypad import KeysScanner
 from kmk.keys import KC
-from kmk.modules.macros import Press, Release, Tap, Macros
+from kmk.modules.macros import Macros, Press, Release, Tap
+from kmk.modules.encoder import EncoderHandler
+from kmk.extensions.media_keys import MediaKeys
 
-# This is the main instance of your keyboard
+keyboard.extensions.append(MediaKeys())
+
 keyboard = KMKKeyboard()
-
-# Add the macro extension
 macros = Macros()
 keyboard.modules.append(macros)
 
-# Define your pins here!
-PINS = [board.D3, board.D4, board.D2, board.D1]
+encoders = EncoderHandler()
+keyboard.modules.append(encoders)
 
-# Tell kmk we are not using a key matrix
+# Define your button pins here (based on the schematic: SW1 to SW6
+KEY_PINS = [
+    board.GPIO1,  # SW1
+    board.GPIO2,  # SW2
+    board.GPIO4,  # SW3
+    board.GPIO3,  # SW4
+    board.GPIO26,   # SW5
+    board.GPIO27,   # SW6
+    board.GPIO29,   # SW7 left (rotary)
+    board.GPIO28,   # SW7 right (rotary)
+]
+
 keyboard.matrix = KeysScanner(
-    pins=PINS,
+    pins=KEY_PINS,
     value_when_pressed=False,
 )
 
-# Here you define the buttons corresponding to the pins
-# Look here for keycodes: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/keycodes.md
-# And here for macros: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/macros.md
-keyboard.keymap = [
-    [KC.A, KC.DELETE, KC.MACRO("Hello world!"), KC.Macro(Press(KC.LCMD), Tap(KC.S), Release(KC.LCMD)),]
+# Define the rotary encoder pins (A, B, and no integrated button pin since push is separate)
+encoders.pins = [
+    (board.GPIO29, board.GPIO28, None),
 ]
 
-# Start kmk!
+# Example keymap for the 7 buttons
+keyboard.keymap = [
+    [
+        # Currently, SW1 and SW5: i haven't assigned anything on purpose!
+        KC.NO, KC.UP, KC.LEFT, KC.DOWN, KC.NO, KC.RIGHT, KC.VOLD, KC.VOLU, 
+    ]
+]
+
+# Example encoder map (one layer, one encoder: clockwise, counterclockwise, button - but button is handled separately)
+encoders.map = [
+    [(KC.VOLU, KC.VOLD, KC.NO)],  # Use KC.NO for button since it's in the keymap
+]
+
 if __name__ == '__main__':
     keyboard.go()
